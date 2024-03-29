fb6bc814c5dd343431de465ac5cba7dc24dc3cd0
Skrywerbeer <liquidsquid1@gmail.com>
2024-03-30 01:35:40 +0200

    Added thumbnails.

--- forays/viewerInit.js
+++ forays/viewerInit.js
@@ -1,7 +1,11 @@
 const viewer = document.querySelector("image-viewer");
-viewer.srcList = viewer.globImageSrcs();
-for (const img of document.querySelectorAll("img"))
+const urls = new Array();
+for (const img of document.querySelectorAll("img")) {
+    const url = img.src.replace("-thumb", "");
+    urls.push(url);
     img.addEventListener("click", () => {
-        viewer.openAt(img.src);
+        viewer.openAt(url);
     });
+}
+viewer.srcList = urls;
 export {};
--- forays/viewerInit.ts
+++ forays/viewerInit.ts
@@ -1,9 +1,14 @@
 import {ImageViewer} from "../components/imageViewer.js";
 
 const viewer = document.querySelector("image-viewer") as ImageViewer;
-viewer.srcList = viewer.globImageSrcs();
 
-for(const img of  document.querySelectorAll("img"))
+
+const urls = new Array();
+for (const img of document.querySelectorAll("img")) {
+	const url = img.src.replace("-thumb", "")
+	urls.push(url);
 	img.addEventListener("click", () => {
-		viewer.openAt(img.src)
+		viewer.openAt(url);
 	});
+}
+viewer.srcList = urls;
